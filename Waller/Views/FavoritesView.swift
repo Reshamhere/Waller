a9984592d3926c515f34ca76d9804a1f97736682
Resham <reshambharti145@gmail.com>
2024-11-13 13:22:59 +0530

    integrated photoview

--- Waller/Views/FavoritesView.swift
+++ Waller/Views/FavoritesView.swift
@@ -16,31 +16,38 @@ struct FavoritesView: View {
         NavigationView {
             List {
                 ForEach(likedPhotos) { likedPhoto in
-//                    NavigationLink(destination: PhotoView(photo: likedPhoto)){}
-                    VStack(alignment: .leading) {
-                        AsyncImage(url: URL(string: likedPhoto.url)) { image in
-                            image
-                                .resizable()
-                                .scaledToFit()
-                        } placeholder: {
-                            ProgressView()
+                    NavigationLink(destination: PhotoView(photo: convertToPhoto(likedPhoto))) {
+                        VStack(alignment: .leading) {
+                            AsyncImage(url: URL(string: likedPhoto.url)) { image in
+                                image
+                                    .resizable()
+                                    .scaledToFit()
+                            } placeholder: {
+                                ProgressView()
+                            }
+                            .frame(maxWidth: .infinity)
+                            .background(Color.black.opacity(0.2))
+                            .cornerRadius(10)
+                            .padding(.vertical, 8)
+                            
+                            Text(likedPhoto.photographer ?? "Unknown Photographer")
+                                .font(.headline)
+                            Text(likedPhoto.alt ?? "No Description")
+                                .font(.subheadline)
+                                .foregroundColor(.secondary)
                         }
-                        .frame(maxWidth: .infinity)
-                        .background(Color.black.opacity(0.2))
-                        .cornerRadius(10)
-                        .padding(.vertical, 8)
-
-                        Text(likedPhoto.photographer ?? "Unknown Photographer")
-                            .font(.headline)
-                        Text(likedPhoto.alt ?? "No Description")
-                            .font(.subheadline)
-                            .foregroundColor(.secondary)
+                        .padding()
                     }
-                    .padding()
                 }
             }
             .navigationTitle("Favorites")
         }
+    } // end of body
+    
+    // Helper function to convert LikedPhoto to Photo
+    private func convertToPhoto(_ likedPhoto: LikedPhoto) -> Photo {
+        return Photo(id: Int(likedPhoto.id) ?? 0, url: "", src: Src(medium: "", large: likedPhoto.url), photographer: likedPhoto.photographer ?? "Unknown", photographerId: likedPhoto.photographerId, alt: likedPhoto.alt, photographerUrl: ""
+        )
     }
 }
 
